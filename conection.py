6d1a15163b1d6afd4e2551f933e8330186c1ab0a
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-06 15:25:50 -0300

    chore: trabalhando na construção do banco de ados

--- conection.py
+++ conection.py
@@ -1,4 +1,16 @@
 import sqlite3
 
 #Conexão com o banco
-conexao = sqlite3.connect("tutorial.db")
+conectar = sqlite3.connect("tutorial.db")
+#Para percorrermos as tabelas
+marcador = conectar.cursor()
+
+marcador.execute("""
+CREATE TABLE tarefas_programacao (
+    id INTEGER PRYMARY KEY.
+    title TEXT NOT NULL, 
+    description TEXT NULL,
+    dificuldade INTEGER NOT NULL CHEACK(dificuldade BETWEEN 1 AND 5).
+    resolvidos INTEGER NOT NULL DEFAULT 0,
+    );
+""")
