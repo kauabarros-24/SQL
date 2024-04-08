514bb5ae1c2499e1ab6b0e7e0d088c2845bb81fd
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-08 11:59:51 -0300

    Outra consulta

--- teste/consulta.py
+++ teste/consulta.py
@@ -6,10 +6,10 @@ marcador = conexao.cursor()
 
 #Reazlizar consulta
 try:
-    marcador.execute('SELECT nome FROM usuario WHERE nome LIKE "%aua";')
-    nome_usuario = marcador.fetchall()
-    for nome in nome_usuario:
-        print(nome)
+    marcador.execute('SELECT email FROM usuario WHERE email LIKE "%.com";')
+    email_usuario = marcador.fetchall()
+    for email in email_usuario:
+        print(email)
 except sqlite3.OperationalError:
     print("sqlite3 não encontrou a tabela")
 
