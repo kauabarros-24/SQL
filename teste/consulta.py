d7ec43cb244c505766db3a0e87d0d1c1b252f736
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-08 11:52:29 -0300

    fixed: Bug da consulta resolvido

--- teste/consulta.py
+++ teste/consulta.py
@@ -1,14 +1,17 @@
 import sqlite3
 
+#Conexao com o banco de dados
 conexao = sqlite3.connect('banco.db')
 marcador = conexao.cursor()
 
-marcador.execute('SELECT nome FROM usuario WHERE nome LIKE "%aua";')
-
-nome_usuarios = marcador.fetchall()
-
-for nome in nome_usuarios:
-    print(nome)
+#Reazlizar consulta
+try:
+    marcador.execute('SELECT nome FROM usuario WHERE nome LIKE "%aua";')
+    nome_usuario = marcador.fetchall()
+    for nome in nome_usuario:
+        print(nome)
+except sqlite3.OperationalError:
+    print("sqlite3 não encontrou a tabela")
 
 marcador.close()
 conexao.close()
